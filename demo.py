5d7d0c78ddeb9412d630f845aba455245bac7c7e
chao-ji <jic@indiana.edu>
2018-01-16 23:52:30 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,8 +1,8 @@
 from lstm import *
 
 train_path = "/home/user/Desktop/lstm_language_model/simple-examples/data/ptb.train.txt"
-test_path = "/home/user/Desktop/lstm_language_model/simple-examples/data/ptb.test.txt"
 valid_path = "/home/user/Desktop/lstm_language_model/simple-examples/data/ptb.valid.txt"
+test_path = "/home/user/Desktop/lstm_language_model/simple-examples/data/ptb.test.txt"
 
 
 class Small(object):
@@ -12,6 +12,7 @@ class Small(object):
   num_layers = 2
   num_steps = 20
   embedding_size = 200
+  hidden_sizes=(200, 200)
   max_epoch = 4
   max_max_epoch = 13
   keep_prob = 1.0 
@@ -19,44 +20,37 @@ class Small(object):
   batch_size = 20
   vocab_size = 10000
 
-class Medium(object):
-  init_scale = 0.05
-  init_lr = 1.0
-  max_grad_norm = 5.
-  num_layers = 2
-  num_steps = 35
-  embedding_size = 650
-  max_epoch = 6
-  max_max_epoch = 39
-  keep_prob = 0.5
-  lr_decay = 0.8
-  batch_size = 20
-  vocab_size = 10000
-
-
 train_config = Small()
 
 test_config = Small()
 test_config.batch_size = 1
-test_config.num_steps = 1
+test_config.num_steps = 1 
 test_config.keep_prob = 1.0
 
-train_df = DataFeeder(filename=train_path, config=train_config)
-test_df = DataFeeder(filename=test_path, config=test_config, vocab=train_df.vocab)
+train_df = DataFeeder(batch_size=train_config.batch_size, num_steps=train_config.num_steps, filename=train_path)
 
 initializer = tf.random_uniform_initializer(-train_config.init_scale,
                                              train_config.init_scale)
 
 sess = tf.InteractiveSession()
-with tf.variable_scope("Model", reuse=None, initializer=initializer):
-  lm = LanguageModel(True, train_df, config=train_config)
-  lm.train(train_df, sess)
 
-tvars = tf.trainable_variables()
-z = sess.run(tvars)
+#with tf.variable_scope("Model", reuse=None, initializer=initializer):
+lm = LanguageModel( init_scale=train_config.init_scale,
+                    init_lr=train_config.init_lr,
+                    max_grad_norm=train_config.max_grad_norm,
+                    num_layers=train_config.num_layers,
+                    num_steps=train_config.num_steps,
+                    embedding_size=train_config.embedding_size,
+                    hidden_sizes=train_config.hidden_sizes,
+                    max_epoch=train_config.max_epoch,
+                    max_max_epoch=train_config.max_max_epoch,
+                    keep_prob=train_config.keep_prob,
+                    lr_decay=train_config.lr_decay,
+                    batch_size=train_config.batch_size,
+                    vocab_size=train_config.vocab_size)
 
+lm.train(train_df, sess)
 
-with tf.variable_scope("Model", reuse=True):
-  lm_test = LanguageModel(False, test_df, config=test_config)
-  p = lm_test.score(test_df, sess)
- 
+test_df = DataFeeder(batch_size=1, num_steps=1, filename=test_path, vocab=train_df.vocab)
+
+p = lm.score(test_df, sess)
