6d8efd1d50c21bc832f105ca685061f73eb1dee3
chao-ji <jic@indiana.edu>
2018-03-03 23:28:30 +0800

    Update model_runner.py

--- model_runner.py
+++ model_runner.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 import data
 import model
@@ -11,8 +12,8 @@ class _BaseModelRunner(object):
     tf.contrib.learn.ModeKeys.validate(type(self).mode)
     self._graph = tf.Graph()
     with self._graph.as_default():
-      self._dataset = data.SequenceDataset(hparams)
 
+      self._dataset = data.LanguageModelDataset(hparams)
       self._tables_initializer = tf.tables_initializer()
       self._tables_initialized = False
 
@@ -37,28 +38,17 @@ class _BaseModelRunner(object):
     return self._model
 
   def restore_params_from(self, sess, ckpt_dir):
-    if not self._tables_initialized:
-      sess.run(self._tables_initializer)
-      self._tables_initialized = True
-
-    latest_ckpt = tf.train.latest_checkpoint(ckpt_dir)
-    if latest_ckpt:
-      print("%s model is loading params from %s..." % (
-          type(self).mode.upper(), latest_ckpt))
-      self._saver.restore(sess, latest_ckpt)
-    else:
-      print("%s model is creating fresh params..." % 
-          type(self).mode.upper())
-      sess.run(self._global_variables_initializer)
+    _restore_params_from(self, sess, ckpt_dir)
 
   def persist_params_to(self, sess, ckpt):
     print("%s model is saving params to %s..." % (
-        type(self).model.upper(), ckpt))
+        type(self).mode.upper(), ckpt))
     self._saver.save(sess, ckpt)
 
 
 class RNNLanguageModelTrainer(_BaseModelRunner):
   mode = tf.contrib.learn.ModeKeys.TRAIN
+
   def __init__(self, builder, hparams):
     super(RNNLanguageModelTrainer, self).__init__(
         builder=builder,
@@ -68,7 +58,6 @@ class RNNLanguageModelTrainer(_BaseModelRunner):
       self.lr = self._get_lr(hparams)
       self.update_op = self._get_update_op(hparams)
     
-
   @property
   def iter_steps(self):
     return self._iter_steps
@@ -90,27 +79,119 @@ class RNNLanguageModelTrainer(_BaseModelRunner):
 
     return update_op
 
-  def _initialize_state(self, hparams, state_val):
-    feed_dict = {}
-    if hparams.unit_type == "lstm":
-      for i, (c, h) in enumerate(self.model.init_state):
-        feed_dict[c] = state_val[i].c
-        feed_dict[h] = state_val[i].h
-    elif hparams.unit_type == "rnn":
-      for i, state in enumerate(self.model.init_state):
-        feed_dict[state] = state_val[i]
+  def train(self, sess, hparams, state):
+    feed_dict = _initialize_state(self, hparams, state)     
 
-    return feed_dict
-
-  def train(self, sess, hparams, state_val):
-    feed_dict = self._initialize_state(hparams, state_val)     
-
-    _, loss, state_val = sess.run([
+    _, loss, state, lr = sess.run([
         self.update_op, 
+        self.model.loss,
+        self.model.final_state,
+        self.lr], feed_dict)
+
+    return loss, state, lr
+
+  def update_iter_steps(self, sess, iter_steps):
+    sess.run(tf.assign(self.iter_steps, iter_steps)) 
+
+
+class RNNLanguageModelEvaluator(_BaseModelRunner):
+  mode = tf.contrib.learn.ModeKeys.EVAL
+
+  def eval(self, sess, hparams, state):
+    feed_dict = _initialize_state(self, hparams, state)
+
+    loss, state = sess.run([
         self.model.loss,
         self.model.final_state], feed_dict)
 
-    return loss, state_val 
+    return loss, state
 
 
+class SequenceGenerator(object):
+  mode = tf.contrib.learn.ModeKeys.INFER
 
+  def __init__(self, builder, hparams):
+    tf.contrib.learn.ModeKeys.validate(type(self).mode)
+    self._graph = tf.Graph()
+    with self._graph.as_default():
+
+      self._dataset = data.GeneratorInput(hparams)
+      self._tables_initializer = tf.tables_initializer()
+      self._tables_initialized = False
+
+      self._model = builder(hparams, self.dataset, type(self).mode)
+
+      self._params = tf.trainable_variables()
+      self._saver = tf.train.Saver(self._params)
+
+  @property
+  def graph(self):
+    return self._graph
+
+  @property
+  def dataset(self):
+    return self._dataset
+
+  @property
+  def model(self):
+    return self._model
+
+  def restore_params_from(self, sess, ckpt_dir):
+    _restore_params_from(self, sess, ckpt_dir)
+
+  def generate_text(self, sess, hparams, primer_seq, length, exclude_ids):
+    item_ids = np.arange(hparams.vocab_size)
+
+    state = sess.run(self.model.init_state)
+    inputs = primer_seq
+
+    out_list = []
+
+    for i in range(length):
+      feed_dict = _initialize_state(self, hparams, state)
+      feed_dict[self.dataset.input_text] = inputs
+      state, logits = sess.run([self.model.final_state, self.model.logits], feed_dict)
+
+      next_id = _sample_next_id(item_ids, logits[0, -1], exclude_ids)
+      out_list.append(self.dataset.reverse_vocab[next_id])
+      inputs = self.dataset.reverse_vocab[next_id]
+    return out_list
+
+def _sample_next_id(item_ids, logits, exclude_ids):
+  while True:
+    id_ = np.random.choice(item_ids, p=_softmax(logits))
+    if id_ not in exclude_ids:
+      return id_
+
+def _softmax(x):
+  ex = np.exp(x - np.max(x))
+  return ex / ex.sum()
+
+def _restore_params_from(runner, sess, ckpt_dir):
+  if not runner._tables_initialized:
+    sess.run(runner._tables_initializer)
+    runner._tables_initialized = True
+
+  latest_ckpt = tf.train.latest_checkpoint(ckpt_dir)
+  if latest_ckpt:
+    print("%s model is loading params from %s..." % (
+        type(runner).mode.upper(), latest_ckpt))
+    runner._saver.restore(sess, latest_ckpt)
+  else:
+    print("%s model is creating fresh params..." %
+        type(runner).mode.upper())
+    sess.run(runner._global_variables_initializer)
+
+def _initialize_state(runner, hparams, state):
+  feed_dict = {}
+  if hparams.unit_type == "lstm":
+    for i, (c, h) in enumerate(runner.model.init_state):
+      feed_dict[c] = state[i].c
+      feed_dict[h] = state[i].h
+  elif hparams.unit_type == "rnn":
+    for i, state in enumerate(self.model.init_state):
+      feed_dict[state] = state[i]
+  return feed_dict
+
+def compute_init_state(runner, sess):
+  return sess.run(runner.model.init_state)
